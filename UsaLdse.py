f646b03a4ab4f0211084bf8a68a080f5d95a4012
Fransueudes <fafreitas.96@gmail.com>
2020-05-06 18:19:32 -0300

    Lista Dinamica Simplesmente Encadeada Python

---
+++ UsaLdse.py
@@ -0,0 +1,17 @@
+import Ldse
+
+l = Ldse.Ldse()
+l.inserirInicio('A')
+l.inserirInicio('B')
+l.inserirFim('C')
+l.show()
+print()
+l.removerFim()
+l.show()
+print()
+l.removerFim()
+l.show()
+print()
+l.removerFim()
+l.show()
+print()
